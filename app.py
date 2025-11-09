cdc16939dec536bcbf6c7b9f2c223bdc72913c43
Luz <luzvelascoreyes@gmail.com>
2025-11-09 06:50:57 -0500

    prueba prototipo AWS

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    # Intenta leer una variable de entorno para demostrar configuración
+
     target = os.environ.get('TARGET', 'Mundo')
-    return f"¡Hola {target}! Esta es la versión 1.0 (Segura)\n"
+    return f"Proyecto {target}. Esta es la versión segura\n"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
